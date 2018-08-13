12a85633165f62a90692ee1151ac5fc6a0e34d9b
owenst <164865+owenst@users.noreply.github.com>
2018-08-13 15:22:17 -0700

    added git_fix_author convenience script

---
+++ git_fix_author.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="164865+owenst@users.noreply.github.com"
+CORRECT_NAME="Trevor"
+CORRECT_EMAIL="trevor@airspace.co"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
