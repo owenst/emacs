c65a28f8569528464a7d69acdc0a5cf92755fba5
owenst <164865+owenst@users.noreply.github.com>
2018-08-20 14:16:55 -0700

    added git config to config file

--- configure.sh
+++ configure.sh
@@ -6,3 +6,7 @@ ln -s .emacs ../.emacs
 ln -s .bash_aliases ../.bash_aliases
 source ../.bash_aliases
 source ../.bashrc
+
+#configure git
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config credential.helper 'cache --timeout 300000'
