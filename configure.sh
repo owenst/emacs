4053ac388c07e2d21c83bca6a8e40dadba590c76
owenst <164865+owenst@users.noreply.github.com>
2018-11-27 14:55:01 -0800

    made OS specific customizations for bashrc

--- configure.sh
+++ configure.sh
@@ -3,6 +3,34 @@
 # run and print commands
 #set -x
 
+
+OS=$(uname -s)
+
+
+case $OS in
+    Darwin)
+	echo "Mac - adding to .bashrc"
+	echo 'if [ -f ~/.bash_aliases ]; then ' >> ~/.bashrc
+	echo '    . ~/.bash_aliases'            >> ~/.bashrc
+	echo 'fi'                               >> ~/.bashrc
+
+	echo "Making .bash_profile find .bashrc"
+
+	echo 'if [ -f ~/.bashrc ]; then'        >> ~/.bash_profile
+	echo '    source ~/.bashrc'             >> ~/.bash_profile
+	echo 'fi'                               >> ~/.bash_profile
+
+	;;
+    
+    Linux)
+	echo "Linux"
+	;;
+    *)
+	echo "Unknown OS"
+esac
+
+# exit 0
+
 ln -s ~/emacs/.emacs ~/.emacs
 ln -s ~/emacs/.bash_aliases ~/.bash_aliases
 ln -s ~/emacs/.bc ~/.bc
@@ -19,5 +47,12 @@ git config user.email "164865+owenst@users.noreply.github.com"
 git config --global alias.hist "log --pretty=format:'%h %ad | [%an] %s%d' --graph --date=short"
 git config credential.helper 'cache --timeout 300000'
 
-echo "run git config -l to see git config"
 
+echo
+echo "Printing git config"
+echo
+git config -l
+echo
+
+echo "run git config -l to see git config"
+echo "check .bash_profile and .bashrc for duplicates"
