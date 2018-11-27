4d000fffbc692f217a6a3e91666484e6d5b00ac9
owenst <164865+owenst@users.noreply.github.com>
2018-11-27 15:19:24 -0800

    added in  to configure.sh

--- configure.sh
+++ configure.sh
@@ -36,9 +36,11 @@ ln -s ~/emacs/.bash_aliases ~/.bash_aliases
 ln -s ~/emacs/.bc ~/.bc
 
 # These should only be run once:
-echo 'set BC_ENV_ARGS=~/.bc; export BC_ENV_ARGS' >> ~/.bashrc
-echo 'export PATH=$PATH:/usr/bin/llvm-config' >> ~/.bashrc
+echo 'set BC_ENV_ARGS=~/.bc; export BC_ENV_ARGS'  >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/bin/llvm-config'     >> ~/.bashrc
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+echo 'export VISUAL=nano'                         >> ~/.bashrc
+echo 'export EDITOR="$VISUAL"'                    >> ~/.bashrc
 source ~/.bashrc
 
 #configure git
