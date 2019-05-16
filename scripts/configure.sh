078329537bf45618c83ea1b23dc26344cc295bc2
owenst <164865+owenst@users.noreply.github.com>
2019-05-15 19:35:52 -0700

    added in pragma thing

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -7,42 +7,56 @@
 OS=$(uname -s)
 
 # exit 0
-
+ONLY_ONCE=
 case $OS in
     Darwin)
-	echo "Mac - adding to .bashrc"
-	echo 'if [ -f ~/.bash_aliases ]; then ' >> ~/.bashrc
-	echo '    . ~/.bash_aliases'            >> ~/.bashrc
-	echo 'fi'                               >> ~/.bashrc
+        if ! grep -q "# ADD ALIASES" ~/.bashrc
+        then
 
-	echo "Making .bash_profile find .bashrc"
+            echo "Mac - adding to .bashrc"
+            echo '# ADD ALIASES'                    >> ~/.bashrc
+            echo 'if [ -f ~/.bash_aliases ]; then ' >> ~/.bashrc
+            echo '    . ~/.bash_aliases'            >> ~/.bashrc
+            echo 'fi'                               >> ~/.bashrc
+        fi
+        if ! grep -q "# FIND BASHRC" ~/.bash_profile
+        then
+            echo "Making .bash_profile find .bashrc"
+            echo '# FIND BASHRC'                    >> ~/.bash_profile
+            echo 'if [ -f ~/.bashrc ]; then'        >> ~/.bash_profile
+            echo '    source ~/.bashrc'             >> ~/.bash_profile
+            echo 'fi'                               >> ~/.bash_profile
+        fi
 
-	echo 'if [ -f ~/.bashrc ]; then'        >> ~/.bash_profile
-	echo '    source ~/.bashrc'             >> ~/.bash_profile
-	echo 'fi'                               >> ~/.bash_profile
+    ;;
 
-	;;
-    
     Linux)
-	echo "Linux"
-	;;
+    echo "Linux"
+    ;;
     *)
-	echo "Unknown OS"
+    echo "Unknown OS"
 esac
 
 
 
 
-echo $PWD
+echo "Current directory: " $PWD
 ln -s $PWD/.bash_aliases ~/.bash_aliases
 ln -s $PWD/.bc ~/.bc
 
 # These should only be run once:
-echo 'set BC_ENV_ARGS=~/.bc; export BC_ENV_ARGS'  >> ~/.bashrc
-echo 'export PATH=$PATH:/usr/bin/llvm-config'     >> ~/.bashrc
-echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
-echo 'export VISUAL=nano'                         >> ~/.bashrc
-echo 'export EDITOR="$VISUAL"'                    >> ~/.bashrc
+if ! grep -q "# ONLY ONCE" ~/.bashrc
+then
+    echo
+    echo "Adding to bashrc"
+    echo
+    echo '# ONLY ONCE'                                >> ~/.bashrc
+    echo 'set BC_ENV_ARGS=~/.bc; export BC_ENV_ARGS'  >> ~/.bashrc
+    echo 'export PATH=$PATH:/usr/bin/llvm-config'     >> ~/.bashrc
+    echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+    echo 'export VISUAL=nano'                         >> ~/.bashrc
+    echo 'export EDITOR="$VISUAL"'                    >> ~/.bashrc
+fi
 source ~/.bashrc
 
 #configure git
@@ -59,4 +73,3 @@ git config -l
 echo
 
 echo "run git config -l to see git config"
-echo "check .bash_profile and .bashrc for duplicates"
